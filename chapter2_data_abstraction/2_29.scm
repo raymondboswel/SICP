48bf0cb6e5ee1e8c304ba945749023f8ce334621
Raymond Boswel <raymondboswel@gmail.com>
2019-11-14 21:51:49 +0200

    Initial work on 2_29

---
+++ chapter2_data_abstraction/2_29.scm
@@ -0,0 +1,32 @@
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (car (cdr mobile)))
+
+(define (branch-length branch)
+  (car branch))
+
+(define x (list (list 1 2) (list 3 4)))
+
+(define (fringe tree)
+  (cond ((null? tree) '())
+        ((not (pair? (car tree)))
+	 (cons (car tree) (fringe (cdr tree))))
+        (else (append (fringe (car tree))
+		    (fringe (cdr tree))))))
+
+(define (total-weight mobile)
+  (define (sum lst)
+    (if (null? (cdr lst))
+	(car lst)
+	(+ (car lst) (sum (cdr lst)))))
+  (let ((weights (fringe mobile)))
+    (sum weights)))
+    
